86bfa6e28f6e66cd5a13048defeaf0e66365d946
Zhangxin <2403728388@qq.com>
2019-12-29 16:49:31 +0800

    小组信息

--- exam/src/views/Home.vue
+++ exam/src/views/Home.vue
@@ -1,21 +1,18 @@
 <template>
   <page-view :title="false">
     <banner/>
-
   </page-view>
 </template>
 
 <script>
 import { PageView } from '../layouts'
 import Banner from './home/Banner'
-import Page1 from './home/Page1'
 
 export default {
   name: 'Home',
   components: {
     PageView,
-    Banner,
-    Page1
+    Banner
   }
 }
 </script>
--- exam/src/views/home/Banner.vue
+++ exam/src/views/home/Banner.vue
@@ -1,30 +1,21 @@
 <template>
   <div class="banner-wrapper">
-    <div class="banner-title-wrapper">
+    <div align="center" class="banner-title-wrapper">
       <div class="title-line-wrapper" style="opacity: 1; transform: translate(0px, 0px);">
         <div class="title-line" style="transform: translateX(-64px);"></div>
       </div>
-      <h1 class="" style="opacity: 1; transform: translate(0px, 0px);">Online Exam</h1>
+      <h1 align="center" class="" style="opacity: 1; transform: translate(0px, 0px);">Online Exam</h1>
       <p style="opacity: 1; transform: translate(0px, 0px);">
         <span>基于SpringBoot+Vue技术栈开发的在线考试系统</span>
       </p>
-      <div class="button-wrapper">
-        <a href="https://github.com/19920625lsg">
-          <a-button type="primary">预览</a-button>
-        </a>
-        <a @click="$router.push({ name: 'docs' })">
-          <a-button style="margin: 0 16px;">开始使用</a-button>
-        </a>
-      </div>
-    </div>
-    <div class="banner-image-wrapper" style="opacity: 1;">
-      <a-carousel arrows autoplay>
-        <div v-for="i in 5" :key="i">
-          <img :src="`/home/cover${i}.jpg`" style="height: 324px"/>
-        </div>
-      </a-carousel>
+      <div style="height:100px"></div>
+      <h1 align="center" class="" style="opacity: 1; transform: translate(0px, 0px);">小组成员</h1>
+      <h2 align="center" class="" style="height:50px; opacity: 1; transform: translate(0px, 0px);">王鸿宇      1552760</h2>
+      <h2 align="center" class="" style="height:50px; opacity: 1; transform: translate(0px, 0px);">张  鑫      1752414</h2>
+      <h2 align="center" class="" style="height:50px; opacity: 1; transform: translate(0px, 0px);">杜  鹏      1652671</h2>
     </div>
   </div>
+
 </template>
 
 <script>
